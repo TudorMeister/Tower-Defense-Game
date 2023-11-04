8c7e2df5a38951cc2b4562d4e704519f74585c51
DragosGhinea <dragos.dev12@gmail.com>
2023-11-04 16:39:13 +0200

    Removing unused field

--- Assets/Scripts/LevelSelect/LevelSelectMapGenerator.cs
+++ Assets/Scripts/LevelSelect/LevelSelectMapGenerator.cs
@@ -5,8 +5,6 @@ public class LevelSelectMapGenerator : MonoBehaviour
 
     public LevelFromCitySelect LevelPopUpSection;
 
-    public Camera UICamera;
-
     // Start is called before the first frame update
     void Start()
     {
