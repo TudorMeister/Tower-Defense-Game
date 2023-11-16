0bff9d8eff55e6bb912a837b56944625f4343c14
DragosGhinea <dragos.dev12@gmail.com>
2023-11-16 20:13:30 +0200

    creating DataManager singleton

--- Assets/Scripts/DataPersistence/DataManager.cs
+++ Assets/Scripts/DataPersistence/DataManager.cs
@@ -3,5 +3,21 @@ using System.Collections.Generic;
 
 public class DataManager
 {
+    private static DataManager _instance;
 
+    public static DataManager Instance
+    {
+        get
+        {
+            _instance ??= new DataManager();
+            return _instance;
+        }
+    }
+
+    private IDataService _dataService = new JsonDataService();
+
+    private DataManager()
+    {
+
+    }
 }
