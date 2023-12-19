7e27429ac0231e6fb7dbea06d35062de1bca4ca9
DragosGhinea <dragos.dev12@gmail.com>
2023-12-19 02:14:23 +0200

    Random spawnpoint selection with random distribution of enemies

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,47 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    public Transform spawnPoint;
-
+    public Transform[] SpawnPoints;
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
     private int waveIndex = 0;
 
-
+    private Transform[] _selectedSpawnPoints;
+    private int[] _selectedSpawnPointsEnemyCount;
 
     // Update is called once per frame
     void Update()
     {
         if (countdown <= 0f)
         {
-            StartCoroutine(SpawnWave());
+            SpawnWave();
             countdown = timeBetweenWaves;
         }
 
-        countdown = countdown - Time.deltaTime;
+        countdown -= Time.deltaTime;
     }
 
+    void UpdateSelectedSpawnPoints()
+    {
+        int numberOfSpawnpoints = SpawnPoints.Length / 2;
+        if (_selectedSpawnPoints == null)
+        {
+            _selectedSpawnPoints = SpawnPoints.Take(numberOfSpawnpoints).ToArray();
+            return;
+        }
+        ShuffleSpawnPoints();
+        _selectedSpawnPoints = SpawnPoints.Take(numberOfSpawnpoints).ToArray();
+    }
 
-
-    IEnumerator SpawnWave()
+    void SpawnWave()
     {
         waveIndex++;
+        UpdateSelectedSpawnPoints();
+        _selectedSpawnPointsEnemyCount = ComputeEnemiesPerSpawnpoint(waveIndex*2);
 
-        for (int i = 0; i < waveIndex; i++) 
+        // for (int i = 0; i < waveIndex; i++) 
+        // {
+        //     SpawnEnemy();
+        //     yield return new WaitForSeconds(0.3f);
+        // }
+
+        for (int i = 0; i < _selectedSpawnPoints.Length; i++)
         {
-            SpawnEnemy();
-            yield return new WaitForSeconds(0.3f);
+            StartCoroutine(SpawnWaveForSpawnPoint(_selectedSpawnPoints[i], _selectedSpawnPointsEnemyCount[i]));
         }
 
     }
 
-    void SpawnEnemy()
+    IEnumerator SpawnWaveForSpawnPoint(Transform SpawnPoint, int NumberOfEnemies)
     {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        for (int i = 0; i < NumberOfEnemies; i++) 
+        {
+            SpawnEnemy(SpawnPoint);
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 
+    void SpawnEnemy(Transform SpawnPoint)
+    {
+        Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+
+
+    void ShuffleSpawnPoints()
+    {
+        // Fisher-Yates shuffle algorithm
+        for (int i = SpawnPoints.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            (SpawnPoints[randomIndex], SpawnPoints[i]) = (SpawnPoints[i], SpawnPoints[randomIndex]);
+        }
+    }
+
+    int[] ComputeEnemiesPerSpawnpoint(int NumberOfEnemies)
+    {
+        int[] toReturn = new int[_selectedSpawnPoints.Length];
+
+        for (int i = 0; i < _selectedSpawnPoints.Length; i++)
+        {
+            // Generate a random number of enemies for each spawn point
+            int enemiesForSpawnPoint = Random.Range(0, NumberOfEnemies);
+            toReturn[i] = enemiesForSpawnPoint;
+            NumberOfEnemies -= enemiesForSpawnPoint;
+        }
+
+        // Distribute any remaining enemies randomly
+        for (int i = 0; i < NumberOfEnemies; i++)
+        {
+            int randomIndex = Random.Range(0, _selectedSpawnPoints.Length);
+            toReturn[randomIndex]++;
+        }
+
+        return toReturn;
+    }
 }
