798a0a7ba8eb151b6fed4b6cda94790933ed3111
Gheorghe Liviu Armand <armi_liv_hh@yahoo.com>
2024-01-05 00:24:01 +0200

    Options menu pentru fiecare nivel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set;}
     public List<Enemy> enemiesList = new List<Enemy>();
+    public GameObject canvas;
+    public CameraControl mainCamera;
+    public BuildManager buildManager;
 
     public void Awake()
     {
@@ -18,4 +22,24 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
     }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.activeSelf == true)
+            {
+                mainCamera.enabled = true;
+                buildManager.enabled = true;
+                canvas.SetActive(false);
+            } else
+            {
+                mainCamera.enabled = false;
+                buildManager.enabled = false;
+                canvas.SetActive(true);
+            }
+        }
+    }
+
 }
